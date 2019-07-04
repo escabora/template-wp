e36f140c1967c7f338471acf46f1a34d0578c06b
escabora_e <gabrielescabora@gmail.com>
2019-07-04 17:01:10 -0300

    ajuste page 404

--- 404.php
+++ 404.php
@@ -2,13 +2,6 @@
 get_header();
 ?>
 
-<section class="section section--breadcrumb">
-  <div class="container">
-    <h2 class="title-section"><?php the_title(); ?></h2>
-    <?php if (function_exists('dimox_breadcrumbs')) dimox_breadcrumbs(); ?> 
-  </div>
-</section>
-
 <div class="container">
     <div class="container__404">
         <?php _e('Oops, essa página não existe!', 'juliafaria'); ?>
